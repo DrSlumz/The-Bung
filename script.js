40bd03208ec1e96573fca778ae2556087442f1af
DrSlumz <weerakan.ron@gmail.com>
2026-01-09 13:58:12 +0700

    add:hover

--- script.js
+++ script.js
@@ -72,3 +72,63 @@ const toggleBackToMenu = () => {
     }
 };
 backFromKollabToMenu.addEventListener('click', toggleBackToMenu);
+
+const hover1 = document.getElementById('hoverproduct1');
+const backside = document.getElementById('productalt1');
+
+const productHover = () => {
+    if (backside.classList.contains('hidden')) {
+        backside.classList.remove('hidden');
+        backside.classList.add('flex');
+    } else {
+        backside.classList.add('hidden');
+        backside.classList.remove('flex');
+    }
+};
+hover1.addEventListener('mouseover', productHover);
+hover1.addEventListener('mouseout', productHover);
+
+const hover2 = document.getElementById('hoverproduct2');
+const backside2 = document.getElementById('productalt2');
+
+const productHover2 = () => {
+    if (backside2.classList.contains('hidden')) {
+        backside2.classList.remove('hidden');
+        backside2.classList.add('flex');
+    } else {
+        backside2.classList.add('hidden');
+        backside2.classList.remove('flex');
+    }
+};
+hover2.addEventListener('mouseover', productHover2);
+hover2.addEventListener('mouseout', productHover2);
+
+const hover3 = document.getElementById('hoverproduct3');
+const backside3 = document.getElementById('productalt3');
+
+const productHover3 = () => {
+    if (backside3.classList.contains('hidden')) {
+        backside3.classList.remove('hidden');
+        backside3.classList.add('flex');
+    } else {
+        backside3.classList.add('hidden');
+        backside3.classList.remove('flex');
+    }
+};
+hover3.addEventListener('mouseover', productHover3);
+hover3.addEventListener('mouseout', productHover3);
+
+const hover4 = document.getElementById('hoverproduct4');
+const backside4 = document.getElementById('productalt4');
+
+const productHover4 = () => {
+    if (backside4.classList.contains('hidden')) {
+        backside4.classList.remove('hidden');
+        backside4.classList.add('flex');
+    } else {
+        backside4.classList.add('hidden');
+        backside4.classList.remove('flex');
+    }
+};
+hover4.addEventListener('mouseover', productHover4);
+hover4.addEventListener('mouseout', productHover4);
